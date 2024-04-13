7223fff5ee41e28a6452068e579908de103b0520
ShaduTiger008 <chatterjeesoumyajeet@gmail.com>
2024-04-14 00:59:59 +0530

    Sql changes

--- server/src/sql/payment.sql
+++ server/src/sql/payment.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS payments (
+CREATE TABLE IF NOT EXISTS ecommerce.payments (
     paymentID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     paymentStatus VARCHAR(255) NOT NULL,
     paymentDate DATE NOT NULL,
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS payments (
     paymentMethod VARCHAR(255) NOT NULL,
     paymentType VARCHAR(255) NOT NULL,
     paymentDescription VARCHAR(255) DEFAULT NULL,
-    paymentCustomerID INT NOT NULL,
+    paymentCustomerID VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     FOREIGN KEY (paymentCustomerID) REFERENCES users(userID)
 );
